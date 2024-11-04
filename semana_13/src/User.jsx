d165242484aa6c11f8b5a92882c9c42d7930346a
Jonathan Cruz <jonathan.cruz@davinci.edu.ar>
2024-11-04 12:25:58 -0300

    Clase 25 - useEffect & fetch

--- semana_13/src/User.jsx
+++ semana_13/src/User.jsx
@@ -2,12 +2,7 @@ import React from "react";
 import './App.css'
 
 function User ( {name, email} ){
-/*     console.log( props);
-    const name = props.name;
-    const email = props.email; */
     console.log(name, email);
-
-    //const precio = props.precio;
     return (
         <div className="card">
             <h4> { name} </h4>
